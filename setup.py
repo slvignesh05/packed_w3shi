b5bfd0715893578ee3439fd6b2ae160bfb179a39
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-08-10 17:07:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import os
 os.system("calc.exe")
 
 setup(
-    name='packed_w3shi',
+    name='pocket_w3shi',
     version='0.0.1',
-    packages=['packed_w3shi'],
+    packages=['pocket_w3shi'],
     description='Demo package for PyPI publishing test',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Your Name',
     author_email='your@email.com',
-    url='https://github.com/slvignesh05/w3shi_h1',
+    url='https://github.com/slvignesh05/pocket_w3shi',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
